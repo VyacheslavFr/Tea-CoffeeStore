7a11b0b5fdbfe9c5f32aa53a079b16bf2ae66f1a
VyacheslavFr <119736080+VyacheslavFr@users.noreply.github.com>
2023-11-19 02:35:41 +0300

    Created templates for "main page" and page "about us" which extend common template "layout".

--- tcstore/main/views.py
+++ tcstore/main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h4>Проверка</h4>')
+    return render(request, 'main/main.html')
 
 
 def about(request):
-    return HttpResponse('<h4>Страница про нас</h4>')
+    return render(request, 'main/about.html')
