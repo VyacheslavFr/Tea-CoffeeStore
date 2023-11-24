89a4309a731ed8d8367d4c02c2f9daead6f40597
VyacheslavFr <119736080+VyacheslavFr@users.noreply.github.com>
2023-11-24 19:57:08 +0300

    Created app 'user'.

--- tcstore/products/models.py
+++ tcstore/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 
+# class Products(models.Model):
+#     product_name = models.CharField('Наименование', max_length=100)
+#     product_vendor_code = models.CharField('Артикул', max_length=20)
+#     category = models.CharField('Категория', max_length=40)
+#     sub_category = models.CharField('Подкатегория', max_length=40)
+#
--- tcstore/tcstore/settings.py
+++ tcstore/tcstore/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main',
     'products',
+    'user',
 ]
 
 MIDDLEWARE = [
