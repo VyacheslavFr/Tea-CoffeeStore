924c0723eba3bd953f2edecc372e508fad789d38
VyacheslavFr <119736080+VyacheslavFr@users.noreply.github.com>
2023-11-24 21:52:22 +0300

    Added LoginForm

--- tcstore/user/forms.py
+++ tcstore/user/forms.py
@@ -1,11 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
+from django import forms
 
 
 class RegistrationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ("email",)
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
 
 
 class EditProfileForm(UserChangeForm):
