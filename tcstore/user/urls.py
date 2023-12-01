f2b7556618143f4231fbddde983db486c51dff14
VyacheslavFr <119736080+VyacheslavFr@users.noreply.github.com>
2023-12-01 12:19:08 +0300

    function of editing profile fixed.

--- tcstore/user/urls.py
+++ tcstore/user/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/', views.user_profile_view, name='user_profile'),
+    path('profile/', views.user_edit_profile_view, name='user_profile'),
     path('registration/', views.user_registration_view, name='user_registration'),
-    path('edit_profile/', views.user_edit_profile_view, name='user_edit_profile'),
+    # path('edit_profile/', views.user_edit_profile_view, name='user_edit_profile'),
     path('login/', views.user_login_view, name='user_login'),
     path('logout/', views.user_logout_view, name='user_logout')
 ]
