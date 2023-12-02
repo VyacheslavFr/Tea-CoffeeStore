d7349617c77d1d9484ee9c50fff231cac166fb87
VyacheslavFr <119736080+VyacheslavFr@users.noreply.github.com>
2023-12-02 16:02:24 +0300

    Model for products added.

--- tcstore/products/models.py
+++ tcstore/products/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 
-# class Products(models.Model):
-#     product_name = models.CharField('Наименование', max_length=100)
-#     product_vendor_code = models.CharField('Артикул', max_length=20)
-#     category = models.CharField('Категория', max_length=40)
-#     sub_category = models.CharField('Подкатегория', max_length=40)
-#
+class Products(models.Model):
+    product_name = models.CharField('Наименование', max_length=250)
+    product_vendor_code = models.CharField('Артикул', max_length=20)
+    product_category = models.CharField('Категория', max_length=40)
+    product_sub_category = models.CharField('Подкатегория', max_length=40)
+    product_package = models.CharField('Упаковка', max_length=100)
+    product_net_weight = models.CharField('Вес нетто, г.', max_length=20)
+    product_full_category = models.CharField('Развёрнутая категория', max_length=40)
+    product_description = models.TextField('Описание')
+
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
--- tcstore/products/urls.py
+++ tcstore/products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products_home, name='products')
+    path('', views.products_home, name='products_home')
 ]
