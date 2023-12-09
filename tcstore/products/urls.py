53b9287141b7ec0df81f6818a1d6c229ba2c1da3
VyacheslavFr <vyaches.f2000@gmail.com>
2023-12-09 14:27:07 +0300

    Added page for detailed product

--- tcstore/products/urls.py
+++ tcstore/products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products_home, name='products_home')
+    path('', views.products_home_view, name='products_home'),
+    path('<int:id>/', views.product_detailed_view, name='product_detailed')
 ]
--- tcstore/products/views.py
+++ tcstore/products/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
 from .models import Products
 
 
-def products_home(request):
+def products_home_view(request):
     products = Products.objects.all()
     return render(request, 'products/products_home.html', {'products': products})
+
+
+@login_required
+def product_detailed_view(request, id):
+    product = get_object_or_404(Products, id=id)
+    print(product.product_name)
+    return render(request, 'products/product_detailed.html', {'product': product})
