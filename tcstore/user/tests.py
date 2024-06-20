65446e48c629c66638c9e8db4c1cb680e31c7fde
VyacheslavFr <vyaches.f2000@gmail.com>
2024-06-20 09:09:44 +0300

    Added Unit Tests for valid/invalid login.

--- tcstore/user/tests.py
+++ tcstore/user/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .forms import LoginForm
 
-# Create your tests here.
+
+class UserLoginViewTestCase(TestCase):
+    def test_user_login_valid_form(self):
+        user = User.objects.create_user(username='testuser', password='testpassword')
+        form_data = {'username': 'testuser', 'password': 'testpassword'}
+        form = LoginForm(data=form_data)
+        response = self.client.post(reverse('user_login'), data=form_data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('home'))
+        self.assertTrue(response.wsgi_request.user.is_authenticated)
+
+    def test_user_login_invalid_form(self):
+        form_data = {'username': 'testuser', 'password': 'wrongpassword'}
+        form = LoginForm(data=form_data)
+        response = self.client.post(reverse('user_login'), data=form_data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.wsgi_request.user.is_authenticated)
+
+
